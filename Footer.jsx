1503bb12637f691a2e06009f78d543ff65d869e5
ironkid3000 <85451493+ironkid3000@users.noreply.github.com>
2021-06-06 23:36:45 +0700

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  var curryear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>copyright @ {curryear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
